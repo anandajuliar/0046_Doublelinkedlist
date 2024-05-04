b0422cbb9487d904d4f54abe154a466350ae5723
anandajuliar <anandajulia001@gmail.com>
2024-05-04 10:46:22 +0700

    step 5 n 6

--- 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
+++ 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
@@ -34,3 +34,6 @@ void addNode()
         {
             START->prev = newNode; // step 4: make the first node to the new node
         }
+        newNode->prev = NULL; // step 5: make the new node point to NULL
+        START = newNode; // step 6: make the new node the first node
+    }
