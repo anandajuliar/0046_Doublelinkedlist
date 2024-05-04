e8668da20885229f321ef066abb66fa82476a978
anandajuliar <anandajulia001@gmail.com>
2024-05-04 10:55:20 +0700

    if

--- 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
+++ 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
@@ -101,3 +101,18 @@ void deleteNode()
         previous = current;
         current = current->next;
     }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+    }
+
+    if (current == START)
+    {
+        START = START->next;
+        if (START != NULL)
+        {
+            START->prev = NULL;
+        }
+    }
