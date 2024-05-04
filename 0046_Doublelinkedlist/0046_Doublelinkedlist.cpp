b9b80d8a1d9185d8532b92643b6ebe9d6ebef227
anandajuliar <anandajulia001@gmail.com>
2024-05-04 10:54:24 +0700

    p delete node

--- 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
+++ 0046_Doublelinkedlist/0046_Doublelinkedlist.cpp
@@ -78,3 +78,20 @@ bool search(int rollNo, Node** previous, Node** current)
     }
     return (*current != NULL);
 }
+
+void deleteNode()
+{
+    Node* previous, * current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;
+
+    if (START == NULL)
+    {
+        cout << "List is emty" << endl;
+        return;
+    }
+
+    current = START;
+    previous = NULL;
